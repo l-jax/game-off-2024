2287e0d3f1f22559840d200454e97956730a230b
Laura Jaques <laura-jaques@outlook.com>
2024-11-17 20:12:00 +0000

    refactor puzzle states

--- GameOff/Assets/Scripts/Puzzles/Puzzle States/CompletedPuzzleState.cs
+++ GameOff/Assets/Scripts/Puzzles/Puzzle States/CompletedPuzzleState.cs
@@ -1,11 +1,12 @@
 public class CompletedPuzzleState : IPuzzleState
 {
     private readonly IPuzzle _puzzle;
-    
+
     public CompletedPuzzleState(IPuzzle puzzle)
     {
         _puzzle = puzzle;
     }
+    
     public void Enter()
     {
         _puzzle.PuzzleComponents.ForEach(p => p.SetEnabled(false));
